a8555682e42770b2e5a975cf7a5fa7bd90948c9b
JeremyMrn <jeremy.meurin@orange.fr>
2020-10-20 11:21:08 +0200

    Ajout de la contrainte email

--- priv/repo/migrations/20201020075142_create_users.exs
+++ priv/repo/migrations/20201020075142_create_users.exs
@@ -2,12 +2,19 @@ defmodule ProjetApi.Repo.Migrations.CreateUsers do
   use Ecto.Migration
 
   def change do
+  
     create table(:users) do
       add :username, :string, null: false
-      add :email, :string
+      add :email, :string, null: false
 
       timestamps()
     end
+    
+    create constraint(
+      :users,
+      :string,
+      check: "email ~ '^([a-zA-Z0-9_\-]+\.)*[a-zA-Z0-9_\-]+@([a-zA-Z0-9_\-]+\.)+([a-zA-Z0-9_\-])'"
+    )
 
   end
 end
